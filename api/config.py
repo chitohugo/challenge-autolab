a06ae145179b98fb8899ad71c667af63736acb47
chitohugo <hugochito@gmail.com>
2021-11-11 09:02:13 -0300

    feat: add reading and inserting script in the db

--- api/config.py
+++ api/config.py
@@ -18,20 +18,20 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DEV_DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(basedir, "dev.sqlite")
+    ) or "sqlite:///" + os.path.join(basedir, "..", "dev.sqlite3")
 
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "TEST_DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(basedir, "test.sqlite")
+    ) or "sqlite:///" + os.path.join(basedir, "..", "test.sqlite3")
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(basedir, "prod.sqlite")
+    ) or "sqlite:///" + os.path.join(basedir, "..", "prod.sqlite3")
 
 
 env_config = {
